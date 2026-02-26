4d6863f85dde4b208a3760e7b279318f289294cb
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-02-26 18:57:28 +0000

    refactored dpad movement

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 
 #include "bn_sprite_items_dot.h"
 #include "bn_sprite_items_square.h"
+#include "movement.h"
 
 // A scaling factor by which to reduce the force applied when orbiting
 // Important for numerical stability
@@ -19,35 +20,6 @@ static constexpr int MAX_ORBITERS = 30;
 // Default starting posiiton and velocity for Orbiter
 static constexpr bn::fixed_point ORBITER_START_POSIITON = {0, 0};
 static constexpr bn::fixed_point ORBITER_START_VELOCITY = {0, 5};
-
-/**
- * Moves sprite up/down/left/right based on the d-pad being held.
- * 
- * Dimensions are moved independently, meaning that diagonal motion is faster than moving in one
- * dimension.
- * 
- * @param sprite the sprite to move
- * @param speed the speed at which to move the sprite in each dimension
- */
-void dPadMoveSprite(bn::sprite_ptr& sprite, bn::fixed speed) {
-    bn::fixed dx = 0;
-    bn::fixed dy = 0;
-
-    if(bn::keypad::left_held()) {
-        dx -= speed;
-    }
-    if(bn::keypad::right_held()) {
-        dx += speed;
-    }
-    if(bn::keypad::up_held()) {
-        dy -= speed;
-    }
-    if(bn::keypad::down_held()) {
-        dy += speed;
-    }
-
-    sprite.set_position(sprite.position() + bn::fixed_point(dx, dy));
-}
 
 /**
  * A center of mass to be orbited around. Position is controlled by the player.
